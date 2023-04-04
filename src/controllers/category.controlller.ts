b4de1f6fee3e9f496350fa87e0814267d1b5e97c
tuannt02 <20522122@gm.uit.edu.vn>
2023-04-04 09:07:09 +0700

    update category route

--- src/controllers/category.controlller.ts
+++ src/controllers/category.controlller.ts
@@ -1,5 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import { Category } from '../models';
+import createError from 'http-errors';
+
+interface categoryModel {
+  id: number;
+  categoryName: string;
+  categoryImage: string;
+}
 
 export const getAllCategoryController = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -10,6 +17,40 @@ export const getAllCategoryController = async (req: Request, res: Response, next
       data: {
         categoryList,
       },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateOneCategoryController = async (
+  req: Request<{}, {}, categoryModel>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id, categoryName, categoryImage } = req.body;
+
+    // Check missing param
+    if (!id || !categoryName || !categoryImage) {
+      throw createError.BadRequest('Missing params');
+    }
+
+    await Category.update(
+      {
+        categoryName,
+        categoryImage,
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    );
+
+    res.status(200).json({
+      status: 200,
+      message: 'Update category successfully',
     });
   } catch (err) {
     next(err);
--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
-import { getAllCategoryController } from '../controllers/category.controlller';
+import {
+  getAllCategoryController,
+  updateOneCategoryController,
+} from '../controllers/category.controlller';
 
 const router: Router = Router();
 
 // [GET] /category/get-all -> Get all category
 router.get('/get-all', getAllCategoryController);
+
+// [POST] /category/update-one -> Update one category
+router.post('/update-one', updateOneCategoryController);
 
 export default router;
