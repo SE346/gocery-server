436103b85c5aceaa83630401af935d66d7b58f20
tuannt02 <20522122@gm.uit.edu.vn>
2023-04-04 15:56:07 +0700

    add one product route

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,6 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 import createError from 'http-errors';
-import { Product } from '../models';
+import { Product, ProductImg } from '../models';
+import { v4 as uuid } from 'uuid';
+
+interface productModel {
+  productId?: string;
+  categoryId: string;
+  name: string;
+  description?: string;
+  imageList: string[];
+  price: number;
+  discount: number;
+  unit: string;
+}
 
 export const getAllProductBelongToCategoryController = async (
   req: Request,
@@ -24,6 +36,42 @@ export const getAllProductBelongToCategoryController = async (
     res.status(200).json({
       status: 200,
       data: productList,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const addOneProductController = async (
+  req: Request<{}, {}, productModel>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { categoryId, name, description, imageList, price, discount, unit } = req.body;
+
+    const newProductId = uuid();
+
+    await Product.create({
+      id: newProductId,
+      categoryId,
+      productName: name,
+      productDescription: description,
+      price,
+      discount,
+      unit,
+    });
+
+    const imageListMapping = imageList.map((curVal) => ({
+      productId: newProductId,
+      imgUrl: curVal,
+    }));
+
+    await ProductImg.bulkCreate(imageListMapping);
+
+    res.status(200).json({
+      status: 200,
+      message: 'Add product successfully',
     });
   } catch (err) {
     next(err);
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
-import { getAllProductBelongToCategoryController } from '../controllers/product.controller';
+import {
+  getAllProductBelongToCategoryController,
+  addOneProductController,
+} from '../controllers/product.controller';
 
 const router: Router = Router();
 
 // [GET] /product/get-product-of-category -> Get all product belong to a category
 router.get('/get-product-of-category', getAllProductBelongToCategoryController);
+
+// [POST] /product/add-one -> Add one product
+router.post('/add-one', addOneProductController);
 
 export default router;
