920a8c775bcc770fbc8e324d7480347f8b4135ec
tuannt02 <20522122@gm.uit.edu.vn>
2023-04-05 20:28:15 +0700

    remove one address route

--- src/controllers/address.controller.ts
+++ src/controllers/address.controller.ts
@@ -1,8 +1,9 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, raw } from 'express';
 import createError from 'http-errors';
 import { Address } from '../models';
 
 interface addressModel {
+  addressId: number;
   name: string;
   provinceId: number;
   provinceName: string;
@@ -76,6 +77,49 @@ export const addNewAddressController = async (
     res.status(200).json({
       status: 200,
       message: 'Add new address successfully',
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const removeAddressController = async (
+  req: Request<{}, {}, addressModel>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { addressId } = req.body;
+
+    if (!addressId) {
+      throw createError.BadRequest('Missing params');
+    }
+
+    // Check if cur address in active status?
+    const curAddress = await Address.findOne({
+      where: {
+        id: addressId,
+      },
+      raw: true,
+    });
+
+    if (!curAddress) {
+      throw createError.BadRequest('Address id not exist');
+    }
+
+    if (curAddress.active) {
+      throw createError.Conflict('This address is in active state, so it cannot be deleted.');
+    }
+
+    await Address.destroy({
+      where: {
+        id: addressId,
+      },
+    });
+
+    res.status(200).json({
+      status: 200,
+      message: 'Remove address successfully',
     });
   } catch (err) {
     next(err);
--- src/routes/address.route.ts
+++ src/routes/address.route.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
   getAllAddressBelongToUserController,
   addNewAddressController,
+  removeAddressController,
 } from '../controllers/address.controller';
 import { verifyAccessToken } from '../utils/jwt_service';
 
@@ -12,5 +13,8 @@ router.get('/get-all', verifyAccessToken, getAllAddressBelongToUserController);
 
 // [POST] /address/add -> Create new address
 router.post('/add', verifyAccessToken, addNewAddressController);
+
+// [DELETE] /address/remove -> Remove one address
+router.delete('/remove', verifyAccessToken, removeAddressController);
 
 export default router;
