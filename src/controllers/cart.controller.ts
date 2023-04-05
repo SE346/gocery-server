3614d0191ea24f22b4d4a1b534109460ee1da422
tuannt02 <20522122@gm.uit.edu.vn>
2023-04-05 15:05:07 +0700

    add product to cart route

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -2,28 +2,80 @@ import { Request, Response, NextFunction } from 'express';
 import createError from 'http-errors';
 import { Cart } from '../models';
 
+interface cartModel {
+  productId: string;
+  quantity: number;
+}
+
 export const getAllProductInCartController = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const { email } = req.body;
-
-    if (!email) {
-      throw createError.BadRequest('Missing params');
-    }
+    // Get userMail from previous middleware
+    const userMail = res.locals.payload.user.mail;
 
     const cartList = await Cart.findAll({
       attributes: { exclude: ['userMail', 'createdAt', 'updatedAt'] },
       where: {
-        userMail: email,
+        userMail,
       },
     });
 
     res.status(200).json({
       status: 200,
       data: cartList,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const addProductToCartController = async (
+  req: Request<{}, {}, cartModel>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // Get userMail from previous middleware
+    const userMail = res.locals.payload.user.mail;
+
+    const { productId, quantity } = req.body;
+
+    if (!productId || !quantity) {
+      throw createError.BadRequest('Missing params');
+    }
+
+    const [cart, created] = await Cart.findOrCreate({
+      where: {
+        userMail,
+        productId,
+      },
+      defaults: {
+        quantity,
+      },
+      raw: true,
+    });
+
+    // Product was existed -> Increase quantity
+    if (!created) {
+      await Cart.update(
+        {
+          quantity: cart.quantity + quantity,
+        },
+        {
+          where: {
+            userMail,
+            productId,
+          },
+        }
+      );
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: 'Product added cart',
     });
   } catch (err) {
     next(err);
--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
-import { getAllProductInCartController } from '../controllers/cart.controller';
+import {
+  getAllProductInCartController,
+  addProductToCartController,
+} from '../controllers/cart.controller';
+import { verifyAccessToken } from '../utils/jwt_service';
 
 const router: Router = Router();
 
 // [GET] /cart/get-all -> Get all product in cart
-router.get('/get-all', getAllProductInCartController);
+router.get('/get-all', verifyAccessToken, getAllProductInCartController);
+
+// [POST] /cart/add -> Add product to cart
+router.post('/add', verifyAccessToken, addProductToCartController);
 
 export default router;
