bc856d18c5542710a3a28d70679b9e12f2201110
tuannt02 <20522122@gm.uit.edu.vn>
2023-04-07 16:22:54 +0700

    update avatar user route

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,14 @@ import { Request, Response, NextFunction } from 'express';
 import createError from 'http-errors';
 import { User } from '../models';
 
+interface userModel {
+  firstName: string;
+  lastName: string;
+  avatar: string;
+  phoneNum: string;
+  dateOfBirth: Date;
+}
+
 export const getAllUserController = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const userList = await User.findAll({
@@ -13,6 +21,41 @@ export const getAllUserController = async (req: Request, res: Response, next: Ne
     res.status(200).json({
       status: 200,
       data: userList,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateAvatarController = async (
+  req: Request<{}, {}, userModel>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // Get userMail from previous middleware
+    const userMail = res.locals.payload.user.mail;
+
+    const { avatar } = req.body;
+
+    if (!avatar) {
+      throw createError.BadRequest('Missing params');
+    }
+
+    User.update(
+      {
+        avatar,
+      },
+      {
+        where: {
+          mail: userMail,
+        },
+      }
+    );
+
+    res.status(200).json({
+      status: 200,
+      message: 'Update avatar successfully',
     });
   } catch (err) {
     next(err);
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { getAllUserController } from '../controllers/user.controller';
+import { getAllUserController, updateAvatarController } from '../controllers/user.controller';
 import { verifyAccessToken, verifyAdminAccessToken } from '../utils/jwt_service';
 
 const router: Router = Router();
 
 // [GET] /user/get-all -> Get all user
 router.get('/get-all', verifyAccessToken, verifyAdminAccessToken, getAllUserController);
+
+// [GET] /user/update-avatar -> Update avatar
+router.post('/update-avatar', verifyAccessToken, updateAvatarController);
 
 export default router;
