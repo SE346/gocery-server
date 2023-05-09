63febc5d30411f128cf7e9332becd1d9d8f016f8
tuannt02 <20522122@gm.uit.edu.vn>
2023-05-09 09:38:48 +0700

    get single comment by id route

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -3,7 +3,7 @@ import { Product, Comment } from '../models';
 import createError from 'http-errors';
 import { ResJSON } from '../utils/interface';
 
-export const getAllCommentBelongToProduct = async (
+export const getAllCommentBelongToProductController = async (
   req: Request<{ productId: string }>,
   res: Response<ResJSON>,
   next: NextFunction
@@ -26,6 +26,31 @@ export const getAllCommentBelongToProduct = async (
       statusCode: 200,
       message: 'Success',
       data: commentList,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getSingleCommentByIdController = async (
+  req: Request<{ commentId: string }>,
+  res: Response<ResJSON>,
+  next: NextFunction
+) => {
+  const { commentId: unconvertCommentId } = req.params;
+  const commentId: number = +unconvertCommentId;
+
+  const comment = await Comment.findByPk(commentId);
+
+  if (!comment) {
+    throw createError.NotFound('Comment with id not found');
+  }
+
+  try {
+    res.status(200).json({
+      statusCode: 200,
+      message: 'Success',
+      data: comment,
     });
   } catch (err) {
     next(err);
--- src/routes/comment.route.ts
+++ src/routes/comment.route.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { verifyAccessToken, verifyAdminAccessToken } from '../utils/jwt_service';
-import { getAllCommentBelongToProduct } from '../controllers/comment.controller';
+import {
+  getAllCommentBelongToProductController,
+  getSingleCommentByIdController,
+} from '../controllers/comment.controller';
 
 const router: Router = Router();
 
 // [GET] /comment/p/{productId} -> Get all comment belong product
-router.get('/p/:productId', verifyAccessToken, getAllCommentBelongToProduct);
+router.get('/p/:productId', verifyAccessToken, getAllCommentBelongToProductController);
+
+// [GET] /comment/{commentId} -> Get single comment by id
+router.get('/:commentId', verifyAccessToken, getSingleCommentByIdController);
 
 export default router;
