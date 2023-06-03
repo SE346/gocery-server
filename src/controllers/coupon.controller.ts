59a2edb96f1ffe97298c48f02dda27a63f9f5c11
tuannt02 <20522122@gm.uit.edu.vn>
2023-06-03 16:35:49 +0700

    get single coupon by id

--- src/controllers/coupon.controller.ts
+++ src/controllers/coupon.controller.ts
@@ -32,3 +32,40 @@ export const getAllCouponController = async (
     next(err);
   }
 };
+
+export const getSingleCouponByIdController = async (
+  req: Request<{ couponId: string }>,
+  res: Response<ResJSON>,
+  next: NextFunction
+) => {
+  try {
+    // Get id & convert to number
+    const { couponId: unconvertCouponId } = req.params;
+    const couponId: number = +unconvertCouponId;
+
+    const coupon = Coupon.findOne({
+      where: {
+        id: couponId,
+      },
+      attributes: {
+        exclude: ['updatedAt', 'createdAt'],
+      },
+      include: [
+        {
+          model: CouponItem,
+          attributes: {
+            exclude: ['updatedAt', 'createdAt'],
+          },
+        },
+      ],
+    });
+
+    res.status(200).json({
+      statusCode: 200,
+      message: 'Success',
+      data: coupon,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
--- src/routes/coupon.route.ts
+++ src/routes/coupon.route.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import { verifyAccessToken, verifyAdminAccessToken } from '../utils/jwt_service';
-import { getAllCouponController } from '../controllers/coupon.controller';
+import {
+  getAllCouponController,
+  getSingleCouponByIdController,
+} from '../controllers/coupon.controller';
 const router: Router = Router();
 
 // [GET] /coupon -> Get all coupon
-router.get('/', verifyAccessToken, verifyAdminAccessToken, getAllCouponController);
+router.get('/', verifyAccessToken, getAllCouponController);
+
+// [GET] /coupon/{couponId} -> Get single coupon by id
+router.get('/:couponId', verifyAccessToken, getSingleCouponByIdController);
 
 export default router;
