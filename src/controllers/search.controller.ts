b6f1c889006b2147b78feba04c26e021a7a88c43
tuannt02 <20522122@gm.uit.edu.vn>
2023-06-06 11:11:15 +0700

    search in category route

--- src/controllers/search.controller.ts
+++ src/controllers/search.controller.ts
@@ -1,8 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-import { Product, Rank, Role, User, UserToRank, UserToRole } from '../models';
+import { Category, Product, Rank, Role, User, UserToRank, UserToRole } from '../models';
 import createError from 'http-errors';
 import { ResJSON } from '../utils/interface';
 import { Op } from 'sequelize';
+import { sequelize } from '../config/sequelize';
 
 type SearchForProductRequest = Request<
   unknown,
@@ -62,6 +63,86 @@ export const searchForProductController = async (
       data: productList,
     });
   } catch (err) {
+    next(err);
+  }
+};
+
+type searchInCategoryRequest = Request<
+  { categoryId: string },
+  unknown,
+  unknown,
+  {
+    limit: string;
+    keyword: string;
+    page: string;
+  }
+>;
+
+export const searchInCategoryController = async (
+  req: searchInCategoryRequest,
+  res: Response<ResJSON>,
+  next: NextFunction
+) => {
+  const t = await sequelize.transaction();
+
+  try {
+    const { limit: unconvertLimit, page: unconvertPage, keyword } = req.query;
+    const { categoryId: unconvertCateId } = req.params;
+    const categoryId: number = +unconvertCateId;
+
+    if (!unconvertLimit || !keyword || !unconvertPage || !categoryId) {
+      throw createError.BadRequest('Missing params');
+    }
+
+    const limit: number = +unconvertLimit;
+    const page: number = +unconvertPage;
+
+    // Check category exist
+    const category = await Category.findByPk(categoryId, {
+      transaction: t,
+    });
+
+    if (!category) {
+      throw createError.Conflict('Category with id not exist');
+    }
+
+    const productList = await Product.findAll({
+      where: {
+        [Op.or]: [
+          {
+            name: {
+              [Op.iLike]: {
+                [Op.any]: [`${keyword}%`, `%${keyword}%`],
+              },
+            },
+          },
+          {
+            description: {
+              [Op.iLike]: {
+                [Op.any]: [`${keyword}%`, `%${keyword}%`],
+              },
+            },
+          },
+        ],
+        categoryId,
+      },
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+      limit,
+      offset: limit * (page - 1),
+      transaction: t,
+    });
+
+    await t.commit();
+
+    res.status(200).json({
+      statusCode: 200,
+      message: 'Success',
+      data: productList,
+    });
+  } catch (err) {
+    await t.rollback();
     next(err);
   }
 };
--- src/routes/search.route.ts
+++ src/routes/search.route.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { verifyAccessToken } from '../utils/jwt_service';
-import { searchForProductController } from '../controllers/search.controller';
+import {
+  searchForProductController,
+  searchInCategoryController,
+} from '../controllers/search.controller';
 
 const router: Router = Router();
 
 // [GET] /search -> Search for product
 router.get('/', verifyAccessToken, searchForProductController);
+
+// [GET] /search/{categoryId} -> Search in category
+router.get('/:categoryId', verifyAccessToken, searchInCategoryController);
 
 export default router;
